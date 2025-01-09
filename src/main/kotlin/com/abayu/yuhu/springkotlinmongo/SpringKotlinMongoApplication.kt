820136fa0beb9f2615935ac2a9976343e0ee97f1
bayu.adinata <bayu.adinata@mindigitalgroup.com>
2025-01-09 11:29:13 +0700

    first commit

---
+++ src/main/kotlin/com/abayu/yuhu/springkotlinmongo/SpringKotlinMongoApplication.kt
@@ -0,0 +1,11 @@
+package com.abayu.yuhu.springkotlinmongo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringKotlinMongoApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringKotlinMongoApplication>(*args)
+}
---
+++ src/test/kotlin/com/abayu/yuhu/springkotlinmongo/SpringKotlinMongoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.abayu.yuhu.springkotlinmongo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringKotlinMongoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
